7c73a83eb9c16523428edfd9b9ef9ac05efe2b4b
Marcel <cisowski@student.agh.edu.pl>
2021-08-31 15:54:19 +0200

    Shooting projectiles and fixing robots

--- Ruby's Adventure/Assets/Scripts/EnemyController.cs
+++ Ruby's Adventure/Assets/Scripts/EnemyController.cs
@@ -7,6 +7,7 @@ public class EnemyController : MonoBehaviour
     public float speed = 3.0f;
     public bool vertical;
     public float changeTime = 3.0f;
+    public bool broken = true;
 
     Rigidbody2D rigidbody2D;
     float timer;
@@ -25,10 +26,20 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!broken)
+        {
+            return;
+        }
+
         timer -= Time.deltaTime;
 
         if (timer < 0)
         {
+            if (!broken)
+            {
+                return;
+            }
+
             direction = -direction;
             timer = changeTime;
         }
@@ -62,5 +73,12 @@ public class EnemyController : MonoBehaviour
         {
             player.ChangeHealth(-1);
         }
+    }
+
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        animator.SetTrigger("Fixed");
     }
 }
